bf3274173fc3dfac4b74dd3018d293d136a3bb72
Catriona Watson <146106882+trustiewatson@users.noreply.github.com>
2023-12-05 17:00:31 +0000

    feat: initial test code

---
+++ frontend/index.js
@@ -0,0 +1,20 @@
+// Define the API URL
+const apiUrl = 'https://localhost:4000/';
+
+const sayHello = () => {
+    // Make a GET request
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
